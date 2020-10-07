3b2f5e6da4242d56fd879ea3386b0695dd166493
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-07 01:42:16 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   passengers.find do |suite, name|
-    suite == :suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       name
   end
 end
